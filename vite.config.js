f9c683882613ecdb04a615f5a19bf5d2af88c300
Lary <zen0718zen@gmail.com>
2025-02-17 01:20:07 +0800

    路徑更改

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // 開發中、產品路徑
-  base: process.env.NODE_ENV === 'production' ? '/ReactWeek5/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/react-week5/' : '/',
   plugins: [react()],
 })
